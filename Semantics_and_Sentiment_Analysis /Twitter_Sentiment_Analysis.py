bca61db00017b27927a084df6a390ac926d2ac7b
sravanneeli <sravan.neeli267@gmail.com>
2021-01-20 16:05:01 +0530

    commit

--- Semantics_and_Sentiment_Analysis /Twitter_Sentiment_Analysis.py
+++ Semantics_and_Sentiment_Analysis /Twitter_Sentiment_Analysis.py
@@ -1,33 +1,10 @@
-import re
 import numpy as np
-import string
-from nltk.corpus import stopwords, twitter_samples
-from nltk.tokenize import TweetTokenizer
-from nltk.stem import PorterStemmer
-from Models.Logistic_Regression import LogisticRegression
+from nltk.corpus import twitter_samples
 from sklearn.model_selection import train_test_split
+from utils import preprocess_tweet
+from Models.Logistic_Regression import LogisticRegression
 
 np.random.seed(1)
-
-# initialize tokenizer
-tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
-en_stopwords = stopwords.words('english')  # load stopwords
-stemmer = PorterStemmer()  # initialize stemmer
-
-
-def preprocess_tweet(tweet):
-    tweet = re.sub(r'RT[\s]+', '', tweet)  # remove retweet string
-    tweet = re.sub(r'https?:\\.*[\r\n]*', '', tweet)  # remove hyperlinks
-    tweet = re.sub(r'#', '', tweet)
-    tweet_tokens = tokenizer.tokenize(tweet)
-    clean_tweet = []
-    for word in tweet_tokens:
-        if (word not in en_stopwords and  # remove stop words
-                word not in string.punctuation):  # remove punctuations
-            stem_word = stemmer.stem(word)  # stemming word
-            clean_tweet.append(stem_word)
-
-    return clean_tweet
 
 
 def load_raw_tweets():
---
+++ Semantics_and_Sentiment_Analysis /utils.py
@@ -0,0 +1,26 @@
+import re
+import string
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import TweetTokenizer
+
+# initialize tokenizer
+tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+en_stopwords = stopwords.words('english')  # load stopwords
+stemmer = PorterStemmer()  # initialize stemmer
+
+
+def preprocess_tweet(tweet):
+    tweet = re.sub(r'RT[\s]+', '', tweet)  # remove retweet string
+    tweet = re.sub(r'https?:\\.*[\r\n]*', '', tweet)  # remove hyperlinks
+    tweet = re.sub(r'#', '', tweet)
+    tweet_tokens = tokenizer.tokenize(tweet)
+    clean_tweet = []
+    for word in tweet_tokens:
+        if (word not in en_stopwords and  # remove stop words
+                word not in string.punctuation):  # remove punctuations
+            stem_word = stemmer.stem(word)  # stemming word
+            clean_tweet.append(stem_word)
+
+    return clean_tweet
